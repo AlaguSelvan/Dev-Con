5e3bf922cb92fb755adeab2d9cb94d352be183c5
AlaguSelvan <34230909+AlaguSelvan@users.noreply.github.com>
2018-05-27 20:54:23 +0530

    validaton sucessfull

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://Selvan:Geralt14@ds237120.mlab.com:37120/mern-app'
+    mongoURI: 'mongodb://Selvan:Geralt14@ds237120.mlab.com:37120/mern-app',
+    secretOrKey: 'secret'
 }
--- models/User.js
+++ models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-var schema = mongoose.schema;
-
+const Schema = require('mongoose').Schema;
 //Create Schema
 const UserSchema = new Schema({
     name: {
@@ -16,8 +15,7 @@ const UserSchema = new Schema({
         required: true
     },
     avatar: {
-        type: String,
-        required: true
+        type: String
     },
     date: {
         type: Date,
