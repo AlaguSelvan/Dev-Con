6bcfb7597946461af4223c4356c890b1f88afa03
Alagu Selvan <alaguselvan97@gmail.com>
2018-06-07 18:58:27 +0530

    lap work

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -1,4 +1,4 @@
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from '../actions/types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_PROFILES } from '../actions/types';
 
 const initialState= {
     profile: null,
@@ -24,7 +24,12 @@ export default function(state = initialState, action){
                 ...state,
                 profile: null
             }
-      
+            case GET_PROFILES:
+            return {
+                ...state,
+                profile: action.payload,
+                loading: false
+            }
         
         default:
         return state;
