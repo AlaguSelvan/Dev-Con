e0ecf46442f5e9020dd912e0528dff17b22d02cd
Selvan <34230909+AlaguSelvan@users.noreply.github.com>
2018-06-19 11:24:41 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://Selvan:Geralt14@ds237120.mlab.com:37120/mern-app',
+    mongoURI: 'mongodb://Selvan:dsfdsfewrwr@ds237120.mlab.com:37120/mern-app',
     secretOrKey: 'secret'
 }
